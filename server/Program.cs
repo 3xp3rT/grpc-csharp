398d6da406ea9bdb2db820b897e9d612f8c377a9
3xp3rT <nakibulshaki@hotmail.com>
2021-07-07 01:52:53 +0600

    Add project files.

---
+++ server/Program.cs
@@ -0,0 +1,42 @@
+﻿using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server
+{
+    class Program
+    {
+        const int port = 500051;
+        static void Main(string[] args)
+        {
+            Server server = null;
+            try
+            {
+                server = new Server()
+                {
+                    Ports = {new ServerPort("loaclhost",port,ServerCredentials.Insecure)}
+                };
+                server.Start();
+                Console.WriteLine("The server is listening on the port: "+port);
+                Console.ReadKey();
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("The server is failed to start: " +e.Message);
+
+                throw;
+            }
+            finally
+            {
+                if (server != null)
+                    server.ShutdownAsync().Wait();
+
+            }
+        }
+    }
+}
