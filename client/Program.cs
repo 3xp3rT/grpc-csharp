c66bc758f4b643748fd7ceb8164673bfdb048bd6
3xp3rT <nakibulshaki@hotmail.com>
2021-07-07 02:10:58 +0600

    add client server

--- client/Program.cs
+++ client/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Grpc.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,20 @@ namespace client
 {
     class Program
     {
+        const string target = "127.0.0.1:50051";
         static void Main(string[] args)
         {
+            Channel channel = new Channel(target, ChannelCredentials.Insecure);
+            channel.ConnectAsync().ContinueWith((task) => {
+                if (task.Status == TaskStatus.RanToCompletion)
+                {
+                    Console.WriteLine("The client connect successfully");
+                }
+            });
+            var client = new DummyService.DummyServiceClient(channel);
+
+            channel.ShutdownAsync().Wait();
+            Console.ReadKey();
         }
     }
 }
